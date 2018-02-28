9316632481018d131f62691630ab099fcf304244
Gérard Baecker <gerard@o34.mooo.com>
2018-02-28 20:42:36 +0100

    Renaming and commenting

--- contracts/KeyRecoverer.sol
+++ contracts/KeyRecoverer.sol
@@ -1,38 +1,33 @@
 pragma solidity 0.4.19;
 
 import "../zeppelin-solidity/contracts/ownership/Ownable.sol";
+import "./KeyRecoverable.sol";
+import "./Whitelist.sol";
 
 
-interface SicosToken {
-
-    function recoverKey(address _oldAddress, address _newAddress) public;
-    function keyRecoverer() public returns (address);
-
-}
-
-
-interface Whitelist {
-
-    function addToWhitelist(address[] _investors) public;
-
-}
-
-
+/// @title SicosToken
+/// @author C+B
 contract KeyRecoverer is Ownable {
 
      // Indices of tokens within array. Note: There's no valid token at index 0.
-    mapping(address => uint) public indices; 
+    mapping(address => uint) public indices;
      // Array of tokens. Note: At index 0 is a placeholder that shouldn't be removed ever.
-    address[] public tokens; 
+    address[] public tokens;
 
+    /// @dev Constructor
     function KeyRecoverer() public {
         tokens.push(address(0));  // Placeholder at index 0.
     }
 
+    /// @dev Check if a token is registered here
+    /// @param _token Ethereum address of token contract instance
+    /// @return True or false
     function containsToken(address _token) public view returns (bool) {
         return indices[_token] > 0;
     }
 
+    /// @dev Register a key recoverable token
+    /// @param _token Ethereum address of token contract instance
     function addToken(address _token) public onlyOwner {
         require(_token != address(0) && !containsToken(_token));
 
@@ -40,6 +35,8 @@ contract KeyRecoverer is Ownable {
         tokens.push(_token);
     }
 
+    /// @dev Unregister a key recoverable token
+    /// @param _token Ethereum address of token contract instance
     function removeToken(address _token) public onlyOwner {
         require(_token != address(0) && containsToken(_token));
 
@@ -55,21 +52,24 @@ contract KeyRecoverer is Ownable {
         delete indices[_token];
     }
 
+    /// @dev Recover key for an investor in all tokens that are registered here
+    /// @param _oldAddress Old Ethereum address of the investor
+    /// @param _newAddress New Ethereum address of the investor
     function recoverKey(address _oldAddress, address _newAddress) public onlyOwner {
         for (uint i=1; i < tokens.length; i++) {
-            if (SicosToken(tokens[i]).keyRecoverer() == address(this)) {
-                SicosToken(tokens[i]).recoverKey(_oldAddress, _newAddress);   
+            if (KeyRecoverable(tokens[i]).keyRecoverer() == address(this)) {
+                KeyRecoverable(tokens[i]).recoverKey(_oldAddress, _newAddress);
             }
-
         }
     }
 
-    function check() public onlyOwner returns (bool) {
+    /// @dev Check if this instance is the keyRecoverer of all registered tokens.
+    /// @return True or false
+    function checkTokens() public view onlyOwner returns (bool) {
         for (uint i=1; i < tokens.length; i++) {
-            if (SicosToken(tokens[i]).keyRecoverer() == address(this)) {
-                return false; 
+            if (KeyRecoverable(tokens[i]).keyRecoverer() == address(this)) {
+                return false;
             }
-
         }
         return true;
     }
