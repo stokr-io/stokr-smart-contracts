ef28b3a20641ab8bab327753fb68abecb2a63849
Gérard Baecker <gerard@o34.mooo.com>
2018-04-23 18:29:59 +0200

    Adapted unit tests to new functionality

--- test/SicosCrowdsale.test.js
+++ test/SicosCrowdsale.test.js
@@ -12,28 +12,34 @@ const { rejectDeploy, rejectTx, now, sleep, duration, currency } = require("./he
 contract("SicosCrowdsale", ([owner,
                              investor1,
                              investor2,
+                             teamAccount,
                              wallet,
                              anyone]) => {
 
     // Helper function to deploy a Whitelist, a SicosToken, and a SicosCrowdsale.
-    const deployWhitelistAndTokenAndCrowdsale = async (startTime, endTime) => {
+    const deployWhitelistAndTokenAndCrowdsale = async (openingTime, closingTime) => {
+        const goal = currency.ether(8);
         const rate = 42;  // 42 tokens per wei
         const cap = currency.ether(10);  // total investments are limited to 10 ether
+        const teamShare = 2525;
         let whitelist = await Whitelist.new({from: owner});
         await whitelist.addAdmin(owner, {from: owner});
         await whitelist.addToWhitelist([investor1, investor2], {from: owner});
         let token = await SicosToken.new(whitelist.address, 0xDEADBEEF, 0xCAFEBABE, {from:owner});
-        let crowdsale = await SicosCrowdsale.new(token.address, startTime, endTime, rate, cap, wallet,
+        let crowdsale = await SicosCrowdsale.new(token.address, openingTime, closingTime,
+                                                 goal, rate, cap, teamShare, wallet,
                                                  {from: owner});
         await token.setMinter(crowdsale.address, {from: owner});
         return [whitelist, token, crowdsale];
     };
 
     describe("deployment", () => {
-        const startTime = now() + duration.days(1);
-        const endTime = startTime + duration.days(1);
+        const openingTime = now() + duration.days(1);
+        const closingTime = openingTime + duration.days(1);
+        const goal = 252525;
         const rate = 2525;
-        const cap = 252525;
+        const cap = 123456;
+        const teamShare = 42;
         let token, crowdsale;
 
         before("requires deployed SicosToken and Whitelist instances", async () => {
@@ -42,39 +48,52 @@ contract("SicosCrowdsale", ([owner,
         });
 
         it("should fail if token address is zero", async () => {
-            await rejectDeploy(SicosCrowdsale.new(0x0, startTime, endTime, rate, cap, wallet,
+            await rejectDeploy(SicosCrowdsale.new(0x0, openingTime, closingTime,
+                                                  goal, rate, cap, teamShare, wallet,
                                                   {from: owner}));
         });
 
-        it("should fail if startTime is in the past", async () => {
-            let _startTime = now() - duration.mins(1);
-            await rejectDeploy(SicosCrowdsale.new(token.address, _startTime, endTime, rate, cap, wallet,
+        it("should fail if openingTime is in the past", async () => {
+            let _openingTime = now() - duration.mins(1);
+            await rejectDeploy(SicosCrowdsale.new(token.address, _openingTime, closingTime,
+                                                  goal, rate, cap, teamShare, wallet,
                                                   {from: owner}));
         });
 
-        it("should fail if endTime is before startTime", async () => {
-            let _endTime = startTime - duration.secs(1);
-            await rejectDeploy(SicosCrowdsale.new(token.address, startTime, _endTime, rate, cap, wallet,
+        it("should fail if closingTime is before openingTime", async () => {
+            let _closingTime = openingTime - duration.secs(1);
+            await rejectDeploy(SicosCrowdsale.new(token.address, openingTime, _closingTime,
+                                                  goal, rate, cap, teamShare, wallet,
+                                                  {from: owner}));
+        });
+
+        it("should fail if goal is zero", async () => {
+            await rejectDeploy(SicosCrowdsale.new(token.address, openingTime, closingTime,
+                                                  0, rate, cap, teamShare, wallet,
                                                   {from: owner}));
         });
 
         it("should fail if rate is zero", async () => {
-            await rejectDeploy(SicosCrowdsale.new(token.address, startTime, endTime, 0, cap, wallet,
+            await rejectDeploy(SicosCrowdsale.new(token.address, openingTime, closingTime,
+                                                  goal, 0, cap, teamShare, wallet,
                                                   {from: owner}));
         });
 
         it("should fail if cap is zero", async () => {
-            await rejectDeploy(SicosCrowdsale.new(token.address, startTime, endTime, rate, 0, wallet,
+            await rejectDeploy(SicosCrowdsale.new(token.address, openingTime, closingTime,
+                                                  goal, rate, 0, teamShare, wallet,
                                                   {from: owner}));
         });
 
         it("should fail if wallet address is zero", async () => {
-            await rejectDeploy(SicosCrowdsale.new(token.address, startTime, endTime, rate, cap, 0x0,
+            await rejectDeploy(SicosCrowdsale.new(token.address, openingTime, closingTime,
+                                                  goal, rate, cap, teamShare, 0x0,
                                                   {from: owner}));
         });
 
         it("should succeed", async () => {
-            crowdsale = await SicosCrowdsale.new(token.address, startTime, endTime, rate, cap, wallet,
+            crowdsale = await SicosCrowdsale.new(token.address, openingTime, closingTime,
+                                                 goal, rate, cap, teamShare, wallet,
                                                  {from: owner});
             let code = await web3.eth.getCode(crowdsale.address);
             assert(code !== "0x" && code !== "0x0", "contract code is expected to be non-zero");
@@ -90,14 +109,19 @@ contract("SicosCrowdsale", ([owner,
             tokenAddress.should.be.bignumber.equal(token.address);
         });
 
-        it("sets correct startTime", async () => {
+        it("sets correct openingTime", async () => {
             let openingTime = await crowdsale.openingTime();
-            openingTime.should.be.bignumber.equal(startTime);
+            openingTime.should.be.bignumber.equal(openingTime);
         });
 
-        it("sets correct endTime", async () => {
+        it("sets correct closingTime", async () => {
             let closingTime = await crowdsale.closingTime();
-            closingTime.should.be.bignumber.equal(endTime);
+            closingTime.should.be.bignumber.equal(closingTime);
+        });
+
+        it("sets correct goal", async () => {
+            let _goal = await crowdsale.goal();
+            _goal.should.be.bignumber.equal(goal);
         });
 
         it("sets correct rate", async () => {
@@ -108,6 +132,11 @@ contract("SicosCrowdsale", ([owner,
         it("sets correct cap", async () => {
             let _cap = await crowdsale.cap();
             _cap.should.be.bignumber.equal(cap);
+        });
+
+        it("sets correct team share", async () => {
+            let _teamShare = await crowdsale.teamShare();
+            _teamShare.should.be.bignumber.equal(teamShare);
         });
 
         it("sets correct wallet address", async () => {
@@ -121,9 +150,9 @@ contract("SicosCrowdsale", ([owner,
         let whitelist, token, crowdsale;
 
         before("deployment", async () => {
-            const startTime = now() + duration.days(1);
-            const endTime = startTime + duration.days(1);
-            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(startTime, endTime);
+            const openingTime = now() + duration.days(1);
+            const closingTime = openingTime + duration.days(1);
+            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(openingTime, closingTime);
         });
 
         it("by anyone is forbidden", async () => {
@@ -154,13 +183,45 @@ contract("SicosCrowdsale", ([owner,
 
     });
 
-    describe("while crowdsale", () => {
+    describe("team account change", () => {
         let whitelist, token, crowdsale;
 
         before("deployment", async () => {
-            const startTime = now() + duration.secs(1);
-            const endTime = startTime + duration.days(1);
-            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(startTime, endTime);
+            const openingTime = now() + duration.days(1);
+            const closingTime = openingTime + duration.days(1);
+            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(openingTime, closingTime);
+        });
+
+        it("by anyone is forbidden", async () => {
+            let teamAccountBefore = await crowdsale.teamAccount();
+            await rejectTx(crowdsale.setTeamAccount(anyone, {from: anyone}));
+            let teamAccountAfter = await crowdsale.teamAccount();
+            teamAccountAfter.should.be.bignumber.equal(teamAccountBefore);
+        });
+
+        it("to zero is forbidden", async () => {
+            let teamAccountBefore = await crowdsale.teamAccount();
+            await rejectTx(crowdsale.setTeamAccount(0x0, {from: owner}));
+            let teamAccountAfter = await crowdsale.teamAccount();
+            teamAccountAfter.should.be.bignumber.equal(teamAccountBefore);
+        });
+
+        it("is possible", async () => {
+            let teamAccountBefore = await crowdsale.teamAccount();
+            await crowdsale.setTeamAccount(anyone, {from: owner});
+            let teamAccountAfter = await crowdsale.teamAccount();
+            teamAccountAfter.should.be.bignumber.equal(anyone);
+        });
+
+    });
+
+    describe("while crowdsale is open", () => {
+        let whitelist, token, crowdsale;
+
+        before("deployment", async () => {
+            const openingTime = now() + duration.secs(1);
+            const closingTime = openingTime + duration.days(1);
+            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(openingTime, closingTime);
             await sleep(duration.secs(2));
             let rate = await crowdsale.rate();
             await crowdsale.setRate(rate, {from: owner});  // Trigger a neutral transaction
@@ -198,13 +259,14 @@ contract("SicosCrowdsale", ([owner,
             balanceAfter.should.be.bignumber.equal(balanceBefore.plus(value.times(rate)));
         });
 
-        it("token purchase increases wallet wei balance", async () => {
+        it("token purchase increases vault wei balance", async () => {
+            let vault = await crowdsale.vault();
             let weiRaisedBefore = await crowdsale.weiRaised();
-            let weiBalanceBefore = await web3.eth.getBalance(wallet);
+            let weiBalanceBefore = await web3.eth.getBalance(vault);
             let value = currency.ether(2);
             await crowdsale.buyTokens(investor1, {from: investor1, value: value});
             let weiRaisedAfter = await crowdsale.weiRaised();
-            let weiBalanceAfter = await web3.eth.getBalance(wallet);
+            let weiBalanceAfter = await web3.eth.getBalance(vault);
             weiRaisedAfter.should.be.bignumber.equal(weiRaisedBefore.plus(value));
             weiBalanceAfter.should.be.bignumber.equal(weiBalanceBefore.plus(value));
         });
@@ -226,13 +288,13 @@ contract("SicosCrowdsale", ([owner,
 
     });
 
-    describe("after crowdsale", () => {
+    describe("after crowdsale was closed", () => {
         let whitelist, token, crowdsale;
 
         before("deployment", async () => {
-            const startTime = now() + duration.secs(1);
-            const endTime = startTime;
-            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(startTime, endTime);
+            const openingTime = now() + duration.secs(1);
+            const closingTime = openingTime;
+            [whitelist, token, crowdsale] = await deployWhitelistAndTokenAndCrowdsale(openingTime, closingTime);
             await sleep(duration.secs(2));
             let rate = await crowdsale.rate();
             await crowdsale.setRate(rate, {from: owner});  // Trigger a neutral transaction
@@ -252,21 +314,44 @@ contract("SicosCrowdsale", ([owner,
             await rejectTx(crowdsale.buyTokens(investor1, {from: investor1, value: currency.wei(1)}));
             let balanceAfter = await token.balanceOf(investor1);
             balanceAfter.should.be.bignumber.equal(balanceBefore);
-        })
+        });
+
+        it("finalization is forbidden if no team account was set", async () => {
+            await rejectTx(crowdsale.finalize({from: owner}));
+            let isFinalized = await crowdsale.isFinalized();
+            isFinalized.should.be.false;
+        });
 
         it("finalization by anyone is forbidden", async () => {
+            await crowdsale.setTeamAccount(teamAccount, {from: owner});
             await rejectTx(crowdsale.finalize({from: anyone}));
             let isFinalized = await crowdsale.isFinalized();
             isFinalized.should.be.false;
         });
 
-        it("finalization is possible and finishes token minting", async () => {
+        it("finalization is forbidden if team account wasn't whitelisted", async () => {
+            await rejectTx(crowdsale.finalize({from: owner}));
+            let isFinalized = await crowdsale.isFinalized();
+            isFinalized.should.be.false;
+        });
+
+        it("finalization is possible", async () => {
+            await whitelist.addToWhitelist([teamAccount], {from: owner});
             let tx = await crowdsale.finalize({from: owner});
             let entry = tx.logs.find(entry => entry.event === "Finalized");
             should.exist(entry);
             let isFinalized = await crowdsale.isFinalized();
-            let mintingFinished = await token.mintingFinished()
             isFinalized.should.be.true;
+        });
+
+        it("finalization mints team share", async () => {
+            let teamShare = await crowdsale.teamShare();
+            let teamBalance = await token.balanceOf(teamAccount);
+            teamBalance.should.be.bignumber.equal(teamShare);
+        });
+
+        it("finalization finshes token minting", async () => {
+            let mintingFinished = await token.mintingFinished();
             mintingFinished.should.be.true;
         });
 
