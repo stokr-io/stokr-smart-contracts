23dcbfd755dce04101541c3ad23f57ffa25c55c3
G. Baecker <admin@o34.mooo.com>
2018-11-05 16:41:23 +0100

    Removed obsolete test scripts

--- scripts/publish.py
+++ scripts/publish.py
@@ -1,73 +1,27 @@
 #!/usr/bin/env python3
 
-"""Helper script to publish contract data into test database.
-
-To upload the current ABIs:
-    python3 publish.py abis
-
-To upload the deployment addresses:
-    python3 publish.py addresses
+"""Helper script to publish contract abis into test database.
 """
 
-import http.client
 import json
 import os
 import os.path
+import requests
 import sys
 
 
-DB_ENDPOINT = "88.198.129.43:8080"
+DB_ENDPOINT = "http://88.198.129.43:8080/abi/"
+CONTRACTS = ["StokrProjectManager",
+             "Whitelist",
+             "StokrToken",
+             "StokrCrowdsale"]
 
-base_dir = os.path.dirname(__file__)
-
-
-def publishAddresses():
-    with open(os.path.join(base_dir, "addresses.json")) as file:
-        contracts = json.load(file)["contracts"]
-    conn = http.client.HTTPConnection(DB_ENDPOINT)
-    for contract, addresses in contracts.items():
-        print("Publishing addresses of", contract)
-        conn.request("PUT", "/addresses/" + contract,
-                     body=json.dumps(addresses),
-                     headers={"Content-Type": "application/json"})
-        resp = conn.getresponse()
-        print(resp.status, resp.reason)
-        body = resp.read()
-        print(body.decode())
-    conn.close()
-    print()
-
-
-def publishABIs():
-    with open(os.path.join(base_dir, "addresses.json")) as file:
-        contracts = json.load(file)["contracts"]
-    conn = http.client.HTTPConnection(DB_ENDPOINT)
-    for contract in contracts:
-        print("Publishing ABI of", contract)
-        with open(os.path.join(base_dir,
-                               "../build/contracts",
-                               contract + ".json")) as file:
-            abi = json.load(file)["abi"]
-        conn.request("PUT", "/abi/" + contract,
-                     body=json.dumps(abi),
-                     headers={"Content-Type": "application/json"})
-        resp = conn.getresponse()
-        print(resp.status, resp.reason)
-        body = resp.read()
-        print(body.decode())
-    conn.close()
-    print()
-
-
-usage_info = f"Usage: {sys.argv[0]} (addresses|abis)"
-
-if len(sys.argv) >= 2:
-    if sys.argv[1] == "addresses":
-        publishAddresses()
-    elif sys.argv[1] == "abis":
-        publishABIs()
-    else:
-        sys.exit(f"Invalid argument. {usage_info}")
-else:
-    sys.exit(f"Missing argument. {usage_info}")
+for contract in CONTRACTS:
+    with open(os.path.join(os.path.dirname(__file__),
+                           "../build/contracts",
+                           contract + ".json")) as file:
+        abi = json.load(file)["abi"]
+    response = requests.put(DB_ENDPOINT + contract, json=abi)
+    print(response.status_code)
+    print(response.text)
 
